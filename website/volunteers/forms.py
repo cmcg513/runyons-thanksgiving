ab7978f746bdaa7fe62bacffffd97248fce6f35f
Casey <cmcg513@gmail.com>
2017-09-24 19:35:50 -0700

    added prelim logic for cpatcha and some validation; EOD: trouble formatting Captcha error message and built-in validation errors also suddenly broken

--- website/volunteers/forms.py
+++ website/volunteers/forms.py
@@ -1,9 +1,21 @@
 from django import forms
+from website.basic.forms import PhoneField
 
 class ContactForm(forms.Form):
     name = forms.CharField(required=True)
-    email = forms.CharField(required=True)
+    email = forms.EmailField(required=True)
+    phone = PhoneField(required=True)
+    number_of_adults = forms.IntegerField(required=True)
+    number_of_children = forms.IntegerField(required=True)
+    preference = forms.CharField(
+        required=True,
+        widget=forms.Select(choices=(
+            ('in_house','in house'),
+            ('driver','driver'),
+            ('either','either')
+            ))
+        )
     body = forms.CharField(
         required=True,
-        widget=forms.Textarea
+        widget=forms.Textarea(attrs={'rows': '5', 'cols': '50'})
     )
--- website/volunteers/views.py
+++ website/volunteers/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from . import forms
-
+from website.settings import RECAPTCHA_URL, RECAPTCHA_PUBLIC_KEY
+from website.local_settings import RECAPTCHA_PRIVATE_KEY
+import requests
+import json
 
 def index(request):
     return redirect('volunteers:about', permanent=True)
@@ -9,14 +12,42 @@ def about(request):
     return render(request, 'volunteers/about.html', {})
 
 def contact(request):
+    error_message='Failed to validate CAPTCHA. Please make sure you check the box below'
     if request.method == 'POST':
+        # print(request.POST)
         form = forms.ContactForm(request.POST)
         if form.is_valid():
-            print(request.POST)
-            return redirect('volunteers:thanks')
+            # import IPython; IPython.embed()
+            # print(request.POST)
+            payload = {
+                'secret': RECAPTCHA_PRIVATE_KEY,
+                'response': request.POST['g-recaptcha-response']
+            }
+            ip = _get_client_ip(request)
+            if ip is not None:
+                payload['remoteip'] = ip
+            resp = requests.post(RECAPTCHA_URL, data=payload)
+            if _successful_captcha(resp):
+                return redirect('volunteers:thanks')
     else:
         form = forms.ContactForm()
-    return render(request, 'volunteers/contact.html', {'form': form})
+    return render(request, 'volunteers/contact.html', {'form': form, 'error_message': error_message})
 
 def thanks(request):
     return render(request, 'volunteers/thanks.html', {})
+
+
+# https://stackoverflow.com/questions/4581789/how-do-i-get-user-ip-address-in-django
+def _get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for is not None:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
+def _successful_captcha(response):
+    if response.status_code == 200:
+        content = json.loads(response.content)
+        return content['success']
+    return False
