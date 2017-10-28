075a30ad52c85ef7dcbb9416c4f1aaf52839135d
Casey <cmcg513@gmail.com>
2017-10-28 12:50:16 -0700

    adding dynamic loading of donation items

--- website/donations/views.py
+++ website/donations/views.py
@@ -1,4 +1,24 @@
 from django.shortcuts import render
+from website.settings import DONATIONS_CSV_PATH, DEBUG
+import csv
+
 
 def index(request):
-    return render(request, 'donations/index.html', {})
+    donations = []
+    try:
+        with open(DONATIONS_CSV_PATH, 'r') as f:
+            reader = csv.reader(f, delimiter=',')
+            for row in reader:
+                if len(row) != 3:
+                    continue
+                donations.append(row)
+    except (csv.Error, FileNotFoundError):
+        pass
+
+    if len(donations) == 0:
+        error_msg = "Unfortunately, it looks like something went wrong loading the list of items... " \
+                    "Check back with us later :)"
+    else:
+        error_msg = None
+
+    return render(request, 'donations/index.html', {'donations': donations, 'error_msg': error_msg})
