912a0e3553d93f6985fcf3c5edfd93ab5b30627d
Casey <cmcg513@gmail.com>
2017-12-26 19:01:41 -0500

    Completed feature - added flag to indicate the event is over - DONE_FOR_THE_YEAR var added - HTML updated to reflect this message - all URLs redirected to index in this scenario

--- website/basic/urls.py
+++ website/basic/urls.py
@@ -1,7 +1,13 @@
 from django.conf.urls import url
 from . import views
+from website.settings import DONE_FOR_THE_YEAR
 
 app_name = 'basic'
+if DONE_FOR_THE_YEAR:
+    regex_pattern = r'^.*$'
+else:
+    regex_pattern = r'^$'
+
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    url(regex_pattern, views.index, name='index')
 ]
--- website/basic/views.py
+++ website/basic/views.py
@@ -1,9 +1,13 @@
 # from django.contrib.auth import authenticate
 from django.shortcuts import render
-from website.settings import EVENT_YEAR
+from website.settings import EVENT_YEAR, DONE_FOR_THE_YEAR
 
 def index(request):
-    return render(request, 'basic/index.html', {'EVENT_YEAR': EVENT_YEAR})
+    return render(request, 'basic/index.html', {
+        'EVENT_YEAR': EVENT_YEAR,
+        'next_year': EVENT_YEAR + 1,
+        'DONE_FOR_THE_YEAR': DONE_FOR_THE_YEAR
+    })
 
 # def login(request):
 #     user = authenticate(
